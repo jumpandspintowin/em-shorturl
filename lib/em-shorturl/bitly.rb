f86320982ea5b359be51e2a6a23aaa1de05c0d6a
Nik Johnson <nikjohnson08@gmail.com>
2013-05-29 11:11:55 -0500

    Improved comments for the bitly driver

--- lib/em-shorturl/bitly.rb
+++ lib/em-shorturl/bitly.rb
@@ -5,12 +5,31 @@ module EventMachine
     module ShortURL
 
         ##
-        # Driver for bitly.com URL Shortening service.
+        # Driver for bitly.com URL Shortening service. Bitly requires API users
+        # to have an account. This can be provided to the driver as either a
+        # pre-obtained access token or as a username and password pair
+        # (optionally with a client id and secret). If a username and password
+        # are provided instead of an access token, login is differed until
+        # needed (first call to +shorten+) or can be explicitly called via the
+        # +login+ function.
 
         class Bitly
             include EventMachine::Deferrable
             API_OAUTH_URL = 'https://api-ssl.bitly.com/oauth/access_token'
             API_SHORTEN_URL = 'https://api-ssl.bitly.com/v3/shorten'
+
+
+            ##
+            # Initialize the driver with account details. Despite being
+            # required by bitly, the driver does not currently verify that
+            # proper account details are given.
+            #
+            # +account+ takes the following options:
+            #   :client_id  The client ID of the registered application
+            #   :client_secret  The secret for the client id
+            #   :username   A username to use for basic authentication
+            #   :password   The password to use for basic authentication
+            #   :token  A previously obtained access token to use
 
             def initialize(account={})
                 @client_id = account[:client_id]
@@ -20,6 +39,11 @@ module EventMachine
                 @access_token = account[:token]
                 @deferrable_args = [self]
             end
+
+
+            ##
+            # Shortens the given URL. If no access token is known, will call
+            # the login function and upon success will call shorten again.
 
             def shorten(url)
                 if @access_token.nil?
@@ -33,6 +57,12 @@ module EventMachine
                 request.errback(&method(:on_error))
                 self
             end
+
+
+            ##
+            # Uses the given username and password to obtain an access token
+            # from bitly. If authentication fails, sets the driver's deferrable
+            # status to failed.
 
             def login
                 params = {
